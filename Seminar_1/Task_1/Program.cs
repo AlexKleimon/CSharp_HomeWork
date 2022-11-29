71c709b279feac653a31296da8f49433f194bb4f
Alex <Utin97@mail.ru>
2022-11-30 00:44:30 +0300

    Moved all folders to a new folder Seminar_1

---
+++ Seminar_1/Task_1/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Задача 1: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+int a = new Random().Next(-9, 10);
+int b = new Random().Next(-9, 10);
+int c = new Random().Next(-9, 10);
+Console.WriteLine("a = {0}, b = {1}, c = {2};", a, b, c);
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+Console.WriteLine("Max = {0}", max);
---
+++ Seminar_1/Task_3/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Задача 3: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8 */
+int N = new Random().Next(2, 21);
+Console.WriteLine("Число N = {0}", N);
+Console.WriteLine("Четыне числа от 1 до {0}: ", N);
+int count = 1;
+while (count <= N)
+{
+    {
+        if ((count % 2) == 0)
+        {
+            Console.WriteLine(count);
+        }
+    }
+    count++;
+}
