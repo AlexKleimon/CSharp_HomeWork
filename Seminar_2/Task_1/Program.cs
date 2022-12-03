019751007d1e2b79d29e96b801f24740e0b37d27
Alex <Utin97@mail.ru>
2022-12-03 18:22:54 +0300

    Added comment Task_1/Program.cs

--- Seminar_2/Task_1/Program.cs
+++ Seminar_2/Task_1/Program.cs
@@ -2,11 +2,11 @@
 456 -> 5
 782 -> 8
 918 -> 1 */
-while (true)
+while (true) 
 {
     Console.WriteLine("Введите трёхзначное число: ");
     int number = int.Parse(Console.ReadLine());
-    if (number > 99 && number < 1000)
+    if (number > 99 && number < 1000) // Трехёзначное ли число?
     {
         int numberTwo = (number % 100) / 10;
         Console.WriteLine($"Второй цифрой числа {number} является {numberTwo}");
