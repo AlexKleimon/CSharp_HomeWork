ac174e60dc497b0488b43780e56155cd94b1c6a5
Alex <Utin97@mail.ru>
2022-12-05 23:19:39 +0300

    fixed code

--- Seminar_2/Task_2/Program.cs
+++ Seminar_2/Task_2/Program.cs
@@ -5,21 +5,27 @@
 78 -> третьей цифры нет
 
 32679 -> 6 */
-Console.WriteLine("Введите число:");
-string numberFull = Console.ReadLine();
-int count = numberFull.Length;
-int numberThree = int.Parse(numberFull);;
-Console.WriteLine(count);
-if (count > 2)
+while (true)
 {
-    for (int index = 3; index < count; index++)
+    Console.WriteLine("Введите число:");
+    string numberFull = Console.ReadLine();
+    int numberThree;
+    int count = numberFull.Length;
+    if (int.TryParse(numberFull, out numberThree)) // проверка: есть ли в переменной numberFull числа
     {
-        numberThree = numberThree / 10;
+        if (count > 2)
+        {
+            for (int index = 3; index < count; index++)
+            {
+                numberThree = numberThree / 10;
+            }
+            numberThree = numberThree % 10;
+            Console.WriteLine($"Третей цифрой числа {numberFull} является: {numberThree}.");
+        }
+        else
+            Console.WriteLine($"У числа {numberFull} нету третей цифры.");
+        break;
     }
-    numberThree = numberThree % 10;
-    Console.WriteLine($"Третей цифрой числа {numberFull} является: {numberThree}.");
 }
-else
-Console.WriteLine($"У числа {numberFull} нету третей цифры.");
 
 
