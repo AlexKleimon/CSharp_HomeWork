2d202063700bdf5e13d30351fd6f2a21f50bccb7
Alex <Utin97@mail.ru>
2022-12-12 23:59:02 +0300

    Added MyMethods.EvenNumber and description methods. Completed Task_34.

--- Seminar_5/MyMethods.cs
+++ Seminar_5/MyMethods.cs
@@ -1,4 +1,18 @@
 public class MyMethods
 {
-    
+    /// <summary>
+    /// Метод подсчета четных чисел в одномерном массиве.
+    /// </summary>
+    /// <param name="array">Одномерный массив чисел.</param>
+    /// <returns>Количество четных чисел в одномерном массиве.</returns>
+    public static int EvenNumbers(int[] array)
+    {
+        int count = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] % 2 == 0)
+                count++;
+        }
+        return count;
+    }
 }
--- Seminar_5/Task_seminar_5.cs
+++ Seminar_5/Task_seminar_5.cs
@@ -1,5 +1,12 @@
+/// <summary>
+/// Задачи 34, 36 и 38.
+/// </summary>
 public class Task_seminar_5
 {
+    /// <summary>
+    /// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+    /// <para>Пример:<br/>[345, 897, 568, 234] -> 2. </para>
+    /// </summary>
     public static void Task_34()
     {
         Console.WriteLine("Введите количество элементов массива: ");
@@ -7,5 +14,7 @@ public class Task_seminar_5
         int[] arry = MyArrayMethods.newArray(numberSize);
         MyArrayMethods.fillArray(100, 1000, arry);
         Console.WriteLine(MyArrayMethods.Print(arry));
+        int count = MyMethods.EvenNumbers(arry);
+        Console.WriteLine($"Количество четных чисел в массиве равно: {count}.");
     }
 }
