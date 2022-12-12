7784a5ae54b84dde8a46bf34a9982f23c862f8d1
Alex <Utin97@mail.ru>
2022-12-12 23:29:24 +0300

    Added flowchart task 34 and MyArrayMethods.cs

---
+++ Seminar_5/MyArrayMethods.cs
@@ -0,0 +1,40 @@
+/// <summary>
+/// Методы работы с массивами.
+/// </summary>
+public class MyArrayMethods
+{
+    /// <summary>
+    /// Метод создания массива.
+    /// </summary>
+    /// <param name="numbArray">Количество элементов в массиве.</param>
+    /// <returns>Массив из numbArray элементов.</returns>
+    public static int[] newArray(int numbArray)
+    {
+        return new int[numbArray];
+    }
+    /// <summary>
+    /// Метод заполнения массива случайными числами.
+    /// </summary>
+    /// <param name="min">Нижняя граница диапазона возможных значений.</param>
+    /// <param name="max">Верхняя граница диапазонна возможных значений.</param>
+    /// <param name="arry">Массив, который необходимо заполнить случайными числами.</param>
+    public static void fillArray(int min, int max, int[] arry)
+    {
+        int size = arry.Length;
+        for (int i = 0; i < size; i++)
+        {
+            arry[i] = Random.Shared.Next(min, max);
+        }
+    }
+    /// <summary>
+    /// Метод записи массива в переменную типа String.
+    /// </summary>
+    /// <param name="arry">Массив, который необходимо записать в переменную типа String.</param>
+    /// <returns>Переменная типа String.</returns>
+    public static string Print(int[] arry)
+    {
+        string Print = String.Join(",", arry);
+        Console.Write("Массив: ");
+        return $"[{Print}]";
+    }
+}
---
+++ Seminar_5/Program.cs
@@ -0,0 +1,4 @@
+﻿int size = 5;
+int[] arry = MyArrayMethods.newArray(size);
+MyArrayMethods.fillArray(100, 1000, arry);
+Console.WriteLine(MyArrayMethods.Print(arry));
